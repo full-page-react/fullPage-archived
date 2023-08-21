ce0a717e665dd64c0d7a9d4c9e3be46e14944942
Soheiljafarnejad <jafarnejad.official@gmail.com>
2023-08-21 11:07:02 +0330

    update landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,36 +5,53 @@ import Link from "next/link";
 import FullPageSection from "@/utils/FullPageSection";
 import FullPageWrapper from "@/utils/FullPageWrapper";
 import Logo from "@/assets/Logo";
+import Pre from "@/common/Pre";
 
 const Home = () => {
   return (
     <FullPageWrapper width="100svw" height="100svh" speed={900}>
       <FullPageSection className="centering flex-col gap-8 bg-gradient-to-br from-[#40adff] to-[#3f62ff]">
         <div className="centering flex-col gap-1 text-white">
-          <h1 className="text-[130px] font-bold mb-4">Full Page</h1>
-          <p className="text-xl">Create Beautiful Fullscreen Scrolling Websites</p>
+          <h1 className="mb-4">Full Page</h1>
+          <h3>Create Beautiful Fullscreen Scrolling Websites</h3>
         </div>
 
-        <div className="centering gap-4">
+        <div className="centering gap-4 text-black">
           <button className="bg-white rounded py-1.5 px-4">View on GitHub</button>
           <button className="bg-white rounded py-1.5 px-4">NPM</button>
         </div>
       </FullPageSection>
       <FullPageSection dir="vertical">
-        <FullPageSection className="bg-gradient-to-br from-[#ff983f] to-[#ff4c40] centering">
-          <div>
-            <h1 className="text-[130px] font-bold">Easy!</h1>
-            <p>
-              fullPage is designed to be easy to use and customize. It includes tens of examples, great documentation and both
-              community and personal support.{" "}
-            </p>
+        <FullPageSection className="bg-gradient-to-br from-[#ff983f] to-[#ff4c40] centering flex-col">
+          <div className="centering !items-stretch flex-1 max-w-[1800px] gap-8">
+            <div>
+              <h2 className="mb-8">Easy and Free !</h2>
+              <h3>
+                fullPage is designed to be easy to use and customize. It includes tens of examples, great documentation and both
+                community and personal support.
+              </h3>
+            </div>
+            <div>
+              <Pre>
+                {` <FullPageWrapper speed={900}>
+     <FullPageSection className="bg-gradient-to-br from-[#40adff] to-[#3f62ff]">
+        <p>Page 1 !</p>
+     </FullPageSection>
+     <FullPageSection className="bg-gradient-to-br from-[#ff983f] to-[#ff4c40]">
+        <p>Page 2 !</p>
+    </FullPageSection>
+ </FullPageWrapper>`}
+              </Pre>
+            </div>
           </div>
-          <div></div>
         </FullPageSection>
       </FullPageSection>
       <FullPageSection>
-        <FullPageSection className="bg-gradient-to-br from-[#f740ff] to-[#553fff]">
-          <LinkElement href="/example/nested" text="Nested" />
+        <FullPageSection className="bg-gradient-to-br from-[#f740ff] to-[#553fff] centering">
+          <div className="centering flex-col gap-2">
+            <h2>Touch me!</h2>
+            <h3>Coming soon ...</h3>
+          </div>
         </FullPageSection>
       </FullPageSection>
     </FullPageWrapper>
